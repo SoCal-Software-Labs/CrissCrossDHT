6195ee5b46cec50bc698d0baadb18223b1910f77
kyle <me@khanson.io>
2022-03-01 17:51:23 -0800

    Add exp2p

--- lib/criss_cross_dht/udp_quick.ex
+++ lib/criss_cross_dht/udp_quick.ex
@@ -1,8 +1,6 @@
 defmodule CrissCrossDHT.UDPQuic do
   import CrissCrossDHT.Server.Utils
 
-  @prefix = <<255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255>>
-
   def open(_ip, _port, dispatcher) do
     ExP2P.Dispatcher.endpoint(dispatcher)
   end
@@ -11,7 +9,7 @@ defmodule CrissCrossDHT.UDPQuic do
     ipstr = tuple_to_ipstr(ip, port)
 
     try do
-      ExP2P.unidirectional_many(endpoint, [ipstr], IO.iodata_to_binary([@prefix, msg]), 10_000)
+      ExP2P.unidirectional_many(endpoint, [ipstr], IO.iodata_to_binary(msg), 10_000)
     rescue
       e -> {:error, e}
     end
